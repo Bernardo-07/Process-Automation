17c68fa3ca6f8ca31631e82acd16bac62f0238e5
Bernardo-07 <bernardoduarte271@gmail.com>
2024-02-23 11:38:12 -0300

    initial commands

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import pyautogui
 import pyperclip
 import time 
 import os
+
+pyautogui.PAUSE = 1
+
+#abre o navegador
+os.system("start msedge.exe")
+time.sleep(5)
+
+#digita o endereço e entra no drive
+pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga?usp=sharing")
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")

a8c267a7f0d6d2bd5d4e89e6953b96eb4435afad
Bernardo-07 <bernardoduarte271@gmail.com>
2024-03-05 10:01:23 -0300

    adjustments

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ pyperclip.copy("https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0
 pyautogui.hotkey("ctrl", "v")
 pyautogui.press("enter")
 
-time.sleep(2)
+time.sleep(6)
 
 #navega até o local do relatório e entra na pasta Exportar
-pyautogui.click(x=171, y=270, clicks=2) #coordenadas encontradas a partir do comando pyautogui.position()
+pyautogui.click(x=367, y=321, clicks=2) #coordenadas encontradas a partir do comando pyautogui.position()
 time.sleep(1)
 
 #faz o donwload do relatório
-pyautogui.click(x=171, y=270)
+pyautogui.click(x=367, y=321)
 time.sleep(1)
-pyautogui.click(x=1097, y=106)
-time.sleep(5)
+pyautogui.click(x=496, y=219)
+time.sleep(6)
 
 tabela = pd.read_excel(r"C://Users/berna/Downloads/Vendas - Dez.xlsx")
 print(tabela)
